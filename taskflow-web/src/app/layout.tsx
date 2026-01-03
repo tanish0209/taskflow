e2e20e7bb5a1ab2bc1fe60197040f1b3c37b0a61
Tanish <bajajtanish0209@gmail.com>
2026-01-03 20:05:07 +0530

    fix:font change

--- taskflow-web/src/app/layout.tsx
+++ taskflow-web/src/app/layout.tsx
@@ -1,14 +1,14 @@
 import type { Metadata } from "next";
-import { Nunito } from "next/font/google";
+import { Plus_Jakarta_Sans } from "next/font/google";
 import "./globals.css";
 import "../styles/calendarOverrides.css";
 
 import { AuthProvider } from "@/context/AuthContext";
 import Script from "next/script";
 
-const noto = Nunito({
+const jakarta = Plus_Jakarta_Sans({
   subsets: ["latin"],
-  weight: ["400", "700", "200", "300", "500", "600", "800", "900"],
+  weight: ["200", "300", "400", "500", "600", "700", "800"],
 });
 
 export const metadata: Metadata = {
@@ -40,7 +40,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={noto.className}>
+      <body className={jakarta.className}>
         <AuthProvider>{children}</AuthProvider>
         {process.env.NODE_ENV === "production" && (
           <>
